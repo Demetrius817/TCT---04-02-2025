5b2bcc85b55b6f22c8ff174c410f697ccc7a2da8
Demetrius817 <155706472+Demetrius817@users.noreply.github.com>
2025-04-02 16:51:05 -0400

    Add files via upload

---
+++ BreathingBOB.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreathingBOB : MonoBehaviour
+{
+    [Header("Configuration")]
+    [SerializeField] private bool _enable = true;
+    [SerializeField, Range(0, 0.1f)] private float _Amplitude = 0.015f; 
+    [SerializeField, Range(0, 30)] private float _frequency = 10.0f;
+    [SerializeField] private Transform _camera = null; 
+    [SerializeField] private Transform _cameraHolder = null;
+
+    [Header("Ground Detection")]
+    [SerializeField] private LayerMask FloorMask;
+    [SerializeField] private Transform FeetTransform;
+
+
+    private float _toggleSpeed = 3.0f;
+    private Vector3 _startPos;
+    private Rigidbody _controller;
+
+    private void Awake()
+    {
+        _controller = GetComponent<Rigidbody>();
+        _startPos = _camera.localPosition;
+    }
+
+    void Update()
+    {
+
+        if (!_enable) return;
+        CheckMotion();
+        ResetPosition();
+       
+    }
+    private Vector3 FootStepMotion()
+    {
+        Vector3 pos = Vector3.zero;
+        pos.y += Mathf.Sin(Time.time * _frequency) * _Amplitude * 2;
+        pos.x += Mathf.Cos(Time.time * _frequency / 2) * _Amplitude * 2;
+        return pos;
+    }
+    private void CheckMotion()
+    {
+        float speed = new Vector3(_controller.velocity.x, 0, _controller.velocity.z).magnitude;
+        if (speed < _toggleSpeed) return;
+        if (!Physics.CheckSphere(FeetTransform.position, 0.1f, FloorMask)) return;
+
+        PlayMotion(FootStepMotion());
+    }
+    private void PlayMotion(Vector3 motion)
+    {
+        _camera.localPosition += motion;
+    }
+
+   
+    private void ResetPosition()
+    {
+        if (_camera.localPosition == _startPos) return;
+        _camera.localPosition = Vector3.Lerp(_camera.localPosition, _startPos, 1 * Time.deltaTime);
+    }
+}
